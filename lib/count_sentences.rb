b68f2709c31222637279f8f37f50553ef19abd17
Luis Herrera <herrera.lrh@gmail.com>
2019-09-02 20:20:57 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,5 +1,4 @@
 require 'pry'
-
 class String
 
   def sentence?
@@ -11,11 +10,10 @@ class String
   end
 
   def exclamation?
-    self.end_with?("!")
+    self.end_with?('!')
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).reject{ |c| c.empty?}.size
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
-
 end
